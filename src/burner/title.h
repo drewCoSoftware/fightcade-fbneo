b028f92ac9f16c0cb63ada6b7a320d191e7993f4
Andrew Ritz <drew@august-harper.com>
2023-06-26 23:29:21 -0400

    - Updated the program title and version data, just for fun :)

--- src/burner/title.h
+++ src/burner/title.h
@@ -1,8 +1,8 @@
 // Define macros for appliction title and description
 #ifdef FBNEO_DEBUG
- #define APP_TITLE "Fightcade FBNeo [DEBUG]"
+ #define APP_TITLE "Fightcade FBNeo - Funscrew Edition [DEBUG]"
 #else
- #define APP_TITLE "Fightcade FBNeo"
+ #define APP_TITLE "Fightcade FBNeo - Funscrew Edition"
 #endif
 
 #define APP_DESCRIPTION "Fightcade FBNeo - Online Retro Gaming"
