cdd1df337b6dd23384c4586c156fba6b4442a05f
Andrew Ritz <drew@august-harper.com>
2023-07-31 23:25:42 -0400

    - Completed this version of the new input dialog.  Seems to be working great!

--- src/burn/version.h
+++ src/burn/version.h
@@ -1,11 +1,10 @@
 // Version number, written as  vV.V.BB  or  vV.V.BBaa
 // (0xVVBBaa, in BCD notation)
-
 #define VER_MAJOR  0
-#define VER_MINOR  2
-#define VER_BETA  99
+#define VER_MINOR  3
+#define VER_BETA  1
 #define VER_ALPHA 0
 
 #define BURN_VERSION (VER_MAJOR * 0x100000) + (VER_MINOR * 0x010000) + (((VER_BETA / 10) * 0x001000) + ((VER_BETA % 10) * 0x000100)) + (((VER_ALPHA / 10) * 0x000010) + (VER_ALPHA % 10))
 
-#define FIGHTCADE_VERSION 54
+#define FIGHTCADE_VERSION 55
--- src/burner/title.h
+++ src/burner/title.h
@@ -1,8 +1,8 @@
 // Define macros for appliction title and description
 #ifdef FBNEO_DEBUG
- #define APP_TITLE "Fightcade FBNeo - Funscrew Edition [DEBUG]"
+ #define APP_TITLE "FC FBNeo - FUNSCREW HBT EDITION! [DEBUG]"
 #else
- #define APP_TITLE "Fightcade FBNeo - Funscrew Edition"
+ #define APP_TITLE "FC FBNeo - FUNSCREW HBT EDITION!"
 #endif
 
 #define APP_DESCRIPTION "Fightcade FBNeo - Online Retro Gaming"
