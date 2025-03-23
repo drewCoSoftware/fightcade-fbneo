8df9ad36fd189e847c9ae4170f375342179b73c2
Andrew A. Ritz <drew@august-harper.com>
2025-03-22 21:47:14 -0400

    - Added all of the files that are normally generated via perl scripts / build events.  Because the output of these scripts is STATIC, there is no reason to require extra tooling (i.e. perl) to constantly regenerate them.  It is actually easier to maintain stuff like the license files + games headers by hand vs. writing a text file AND a perl script that squirts out an RTF that you could have just edited in the first place.  Note that only the VS-2022 project is actually setup to use these new static files.

---
+++ src/generated/build_details.h
@@ -0,0 +1,8 @@
+#define BUILD_TIME 12:44:08
+#define BUILD_DATE Aug  4 2024
+#define BUILD_CHAR ANSI
+#define BUILD_CPU  X86
+#define BUILD_COMP Visual C++ 13.4
+#ifndef _MSC_VER
+ #define _MSC_VER  1940
+#endif
